81fc0ba7fba2bd8cc1958b1b3da0ea4b423f4585
ErrorCodeTaken <jraguib@gmail.com>
2026-05-01 14:58:03 -0600

    aaagh I dont event know
    Ill be more careful from here on

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var actions = []
 var is_busy = false
 var selected_plant = ""
 var current_request = ""
@@ -52,18 +53,107 @@ func _on_deliver_pressed() -> void:
 	var correct = false
 	
 	if current_request == "headache" and selected_plant == "A":
-		correct = true
-	elif current_request == "rash" and selected_plant == "B":
-		correct = true
+		var has_boil = "boil" in actions
+		var great = false
+	
+		if has_boil:
+			var boil_index = actions.find("boil")
+			var crushed_before = false
 		
-	if correct:
-		$Label.text = "Good Choice!"
-	else:
-		$Label.text = "That didn't help..."
+			for i in range(boil_index):
+				if actions[i] == "crush":
+					crushed_before = true
+		
+			if crushed_before:
+				great = true
+	
+		if has_boil:
+			if great:
+				$Label.text = "Great choice!"
+			else:
+				$Label.text = "It helped, but weak"
+		else:
+			$Label.text = "That didn't help..."
+	
+		selected_plant = ""
+		actions.clear()
+		$SelectionLabel.text = "Actions: None"
+		await get_tree().create_timer(1.5).timeout
+		show_request()
+		is_busy = false
+		return
+	
+	if current_request == "rash" and selected_plant == "B":
+		var has_cut = "cut" in actions
+		var has_crush = "crush" in actions
+		var has_boil = "boil" in actions
+
+		var result = "That didn't help..."
+
+		if has_boil:
+			# Boiling damages aloe
+			if has_cut:
+				result = "It helped, but weak"
+			else:
+				result = "That didn't help..."
+		else:
+			if has_cut and has_crush:
+				result = "Good treatment"
+			elif has_cut:
+				result = "It helped"
+			elif has_crush:
+				result = "That didn't help..."
+			else:
+				result = "That didn't help..."
+
+		$Label.text = result
+
+		selected_plant = ""
+		actions.clear()
+		$SelectionLabel.text = "Actions: None"
+
+		await get_tree().create_timer(1.5).timeout
+		show_request()
+		is_busy = false
+		return
+	
 		
 	selected_plant = ""
+	actions.clear()
 	$SelectionLabel.text = "Selected: None"
 	await get_tree().create_timer(1.5).timeout
 	show_request()
 	is_busy = false
 		
+
+
+func _on_crush_pressed() -> void:
+	if is_busy:
+		return
+	
+	actions.append("crush")
+	$SelectionLabel.text = "Actions: " + ", ".join(actions).capitalize()
+
+
+func _on_boil_pressed() -> void:
+	if is_busy:
+		return
+	
+	actions.append("boil")
+	$SelectionLabel.text = "Actions: " + ", ".join(actions).capitalize()
+
+
+func _on_cut_pressed() -> void:
+	if is_busy:
+		return
+	
+	actions.append("cut")
+	$SelectionLabel.text = "Actions: " + ", ".join(actions).capitalize()
+
+
+func _on_clear_actions_pressed() -> void:
+	if is_busy:
+		return
+	
+	actions.clear()
+	$SelectionLabel.text = "Actions: None"
